0819a089415ff030849bc738d643e5ebe4d0b22c
Almir Tavares <eng.almirtavares@gmail.com>
2022-09-29 19:47:43 -0300

    Initial Commit

---
+++ AppVIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppVIPER
+//
+//  Created by almir.tavares on 29/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
